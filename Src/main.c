7e8673c147023921db48c0bc0c7aa8dc5d7a69ed
roozbeh bazargani <roozbehbazargani@gmail.com>
2019-08-25 13:47:58 +0430

    Receiving pins corrected

--- Src/main.c
+++ Src/main.c
@@ -23,7 +23,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "string.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -44,16 +44,23 @@
 /* Private variables ---------------------------------------------------------*/
 TIM_HandleTypeDef htim3;
 
+UART_HandleTypeDef huart1;
+
 /* USER CODE BEGIN PV */
 
 uint16_t count;
 uint32_t data;
+
+uint16_t duration[300];
+uint8_t err = 0;
+uint8_t dataa[4];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM3_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 void uDelay(const uint16_t d);
@@ -95,6 +102,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_TIM3_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -106,21 +114,42 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-		while (HAL_GPIO_ReadPin (GPIOA, GPIO_PIN_1));   // wait for the pin to go low
-		while (!(HAL_GPIO_ReadPin (GPIOA, GPIO_PIN_1)));  // wait for the pin to go high.. 9ms LOW
-		while (HAL_GPIO_ReadPin (GPIOA, GPIO_PIN_1));   // wait for the pin to go low
+		/*for(uint8_t i=0;i<4;i++)
+		{
+		for(uint8_t j=0;j<8;j++)
+		{
+		dataa[i]|= data & (1UL << (31-j));
+		}
+		data=data &(1UL << (31-i*8));
+	 }*/
+		//we can only transmit 1byte at a time
+		/*data1=data & 11111111000000000000000000000000;
+		data2=data & 00000000111111110000000000000000;
+		data3=data & 00000000000000001111111100000000;
+		data4=data & 00000000000000000000000011111111;
+		//transmitting from stm to the laptop serial terminal
+		HAL_UART_transmit(&huart2 , *data1 , strlen(*data1) ,100);
+		HAL_UART_transmit(&huart2 , *data2 , strlen(*data2) ,100);
+		HAL_UART_transmit(&huart2 , *data3 , strlen(*data3) ,100);
+		HAL_UART_transmit(&huart2 , *data4"\r\n" , strlen(*data4"\r\n") ,100);
+		HAL_Delay(500);*/
+		
+		
+		while (HAL_GPIO_ReadPin (GPIOA, GPIO_PIN_8));   // wait for the pin to go low
+		while (!(HAL_GPIO_ReadPin (GPIOA, GPIO_PIN_8)));  // wait for the pin to go high.. 9ms LOW
+		while (HAL_GPIO_ReadPin (GPIOA, GPIO_PIN_8));   // wait for the pin to go low
 		for (int i=0; i<32; i++)
 	  {
 		  count=0;
  
-		  while (!(HAL_GPIO_ReadPin (GPIOA, GPIO_PIN_1))); // wait for pin to go high.. this is 562.5us LOW
+		  while (!(HAL_GPIO_ReadPin (GPIOA, GPIO_PIN_8))); // wait for pin to go high.. this is 562.5us LOW
  
-		  while ((HAL_GPIO_ReadPin (GPIOA, GPIO_PIN_1)))  // count the space length while the pin is high
+		  while ((HAL_GPIO_ReadPin (GPIOA, GPIO_PIN_8)))  // count the space length while the pin is high
 		  {
 			  count++;
 			  uDelay(100);
 		  }
- 
+			
 		  if (count > 12) // if the space is more than 1.2 ms
 		  {
 			  data |= (1UL << (31-i));   // write 1
@@ -128,6 +157,13 @@ int main(void)
  
 		  else data &= ~(1UL << (31-i));  // write 0
 	  }
+		
+		HAL_UART_transmit(&huart1 , *data , strlen(*data) ,100);
+		
+		HAL_UART_transmit(&huart1 , *dataa[2] , strlen(dataa[2]) ,100);
+		HAL_UART_transmit(&huart1 , *dataa[3] , strlen(dataa[3]) ,100);
+		HAL_UART_transmit(&huart1 , *dataa[4]"\r\n" , strlen(dataa[4]), "\r\n" ,100);
+		HAL_UART_transmit(&huart1 , "salam\r\n" , strlen("salam\r\n") ,100);
   }
   /* USER CODE END 3 */
 }
@@ -140,6 +176,7 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Initializes the CPU, AHB and APB busses clocks 
   */
@@ -160,6 +197,12 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
+  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
     Error_Handler();
   }
@@ -207,6 +250,41 @@ static void MX_TIM3_Init(void)
   /* USER CODE BEGIN TIM3_Init 2 */
 
   /* USER CODE END TIM3_Init 2 */
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 38400;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
 
 }
 
